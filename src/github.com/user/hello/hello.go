a1f61d22fd4fed12692de111c9996db576d6e99d
NeilS42 <neil.shepherd@sky.uk>
2015-02-25 17:04:38 +0000

    End of day

--- src/github.com/user/hello/hello.go
+++ src/github.com/user/hello/hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+
+	"fmt"
+
+	"github.com/user/stringutil"
+)
+
+
 
 func main() {
-    fmt.Printf("hello, world 3\n")
+    fmt.Printf(stringutil.Reverse("hello, world reversed"))
 }
